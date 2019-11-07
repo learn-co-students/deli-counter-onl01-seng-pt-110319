758bc86ebf7f2d0002e200536f95a3e7edd32a25
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-07 23:14:47 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def line (currentQueue)
+
+  if currentQueue.size == 0
+    return puts "The line is currently empty."
+  else
+    index = 0
+    message = "The line is currently: "
+    while index < currentQueue.size do
+      if index == (currentQueue.size - 1)
+        message += "#{index+1}. #{currentQueue[index]}"
+      else
+        message += "#{index+1}. #{currentQueue[index]} "
+      end
+      index += 1
+    end
+  end
+  puts message
+end
+
+def take_a_number(katz_deli, name)
+  
+  
+end
